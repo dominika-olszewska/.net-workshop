803e79ebfaab1c488352f833ade8dbec84fbb062
Dominika Nowak <dolszewska1@sii.pl>
2024-05-21 09:56:54 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace workshop.training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
